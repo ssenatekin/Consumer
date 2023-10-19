89ebf2de6d893fe51e75e813d87ea9cc6496ab08
ssenatekin <sado-tekin@hotmail.com>
2023-10-19 13:08:41 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory= new ConnectionFactory{
+    HostName="localhost"
+};
+var connection=factory.CreateConnection();
+using var channel=connection.CreateModel();
+
+channel.QueueDeclare("student",exclusive:false,autoDelete:false);//bu queue yoksa oluşturur
+
+var consumer=new EventingBasicConsumer(channel);
+consumer.Received += Receiver;
+
+channel.BasicConsume(queue:"student",consumer:consumer);
+Console.ReadLine();
+
+//event için receiver adındaki methodu delege ediyoruz, yetkilendiriyorum
+void Receiver(object model,BasicDeliverEventArgs eventArgs){
+    var body=eventArgs.Body.ToArray();
+    var message=Encoding.UTF8.GetString(body);
+    System.Console.WriteLine($"Received{message}");
+}
+
